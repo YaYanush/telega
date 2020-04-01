04622ca86ec02f1427d361638e73289c79bac8d2
Yahor <yegor.yanush@gmail.com>
2020-04-01 17:02:32 +0300

    1

--- src/App.js
+++ src/App.js
@@ -4,21 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <p class="from-me">Hey there! What's up</p>
+      <p class="from-them">Checking out iOS7 you know..</p>
     </div>
   );
 }
